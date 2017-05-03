3626ac549181942f65b5233588b116234c11999a
dgketchum <dgketchum@gmail.com>
2017-05-03 14:11:44 -0600

    push for Jake

--- landsat/usgs_setup.py
+++ landsat/usgs_setup.py
@@ -56,86 +56,22 @@ def connect_earth_explorer(usgs):
 
 def download_chunks(url, out_dir, tgz_file):
 
-    r = requests.get(url, stream=True)
-
     local_file = os.path.join(out_dir, tgz_file)
+    print 'url: {}'.format(url)
+    print 'file: {}'.format(local_file)
+
+    r = requests.get(url, stream=True)
 
     if r.status_code == 200:
         with open(local_file, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024 * 1024 * 10):
+                print chunk
                 f.write(chunk)
-
-        return local_file
-
-# def download_chunks(url, rep, nom_fic):
-#     """ Downloads large files in pieces
-#    inspired by http://josh.gourneau.com
-#   """
-#     print 'download chunks: {}, {}, {}'.format(url, rep, nom_fic)
-#     try:
-#         req = urllib2.urlopen(url)
-#         # if downloaded file is html
-#         if req.info().gettype() == 'text/html':
-#             print "error : file is in html and not an expected binary file"
-#             lines = req.read()
-#             if lines.find('Download Not Found') > 0:
-#                 raise TypeError
-#             else:
-#                 with open("error_output.html", "w") as f:
-#                     f.write(lines)
-#                     print "result saved in ./error_output.html"
-#                     # sys.exit(-1)
-#         # if file too small
-#         total_size = int(req.info().getheader('Content-Length').strip())
-#
-#         if (total_size < 50000):
-#             print "Error: The file is too small to be a Landsat Image"
-#             print url
-#             # sys.exit(-1)
-#         print nom_fic, total_size
-#         total_size_fmt = sizeof_fmt(total_size)
-#
-#         # download
-#         downloaded = 0
-#         CHUNK = 1024 * 1024 * 8
-#         with open(rep + '/' + nom_fic, 'wb') as fp:
-#             start = time.clock()
-#             print('Downloading {0} ({1}):'.format(nom_fic, total_size_fmt))
-#             while True:
-#                 chunk = req.read(CHUNK)
-#                 downloaded += len(chunk)
-#                 done = int(50 * downloaded / total_size)
-#                 sys.stdout.write('\r[{1}{2}]{0:3.0f}% {3}ps'
-#                                  .format(math.floor((float(downloaded)
-#                                                      / total_size) * 100),
-#                                          '=' * done,
-#                                          ' ' * (50 - done),
-#                                          sizeof_fmt((downloaded // (time.clock() - start)) / 8)))
-#                 sys.stdout.flush()
-#                 if not chunk: break
-#                 fp.write(chunk)
-#     except urllib2.HTTPError, e:
-#         if e.code == 500:
-#             pass  # File doesn't exist
-#         else:
-#             print "HTTP Error:", e.code, url
-#         return False
-#     except urllib2.URLError, e:
-#         print "URL Error:", e.reason, url
-#         return False
-#     print 'unknown French words rep: {}, nom fic: {}'.format(rep, nom_fic)
-#     return rep, nom_fic
-
-
-def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-            # num /= 1024.0
 
 
 def unzip_image(tgzfile, outputdir):
     target_tgz = os.path.join(outputdir, tgzfile)
+    print 'target tgz: {}'.format(target_tgz)
     if os.path.exists(target_tgz):
         tfile = tarfile.open(target_tgz, 'r:gz')
         tfile.extractall(outputdir)
@@ -261,6 +197,7 @@ def get_candidate_scenes_list(path_row, sat_name, start_date, end_date):
 
 
 def down_usgs_by_list(scene_list, output_dir, usgs_creds_txt):
+
     usgs_creds = get_credentials(usgs_creds_txt)
     connect_earth_explorer(usgs_creds)
 
@@ -271,7 +208,6 @@ def down_usgs_by_list(scene_list, output_dir, usgs_creds_txt):
         url = '{}{}'.format(base_url, tail_string)
 
         tgz_file = '{}.tgz'.format(product)
-        print 'download chunks: {}, {}, {}'.format(url, output_dir, tgz_file)
         download_chunks(url, output_dir, tgz_file)
         print 'image: {}'.format(os.path.join(output_dir, tgz_file))
         unzip_image(tgz_file, output_dir)
