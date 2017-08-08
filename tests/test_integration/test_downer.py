74e87ae32e16d57b23cdcfcbb72f65df9d1705be
dgketchum <dgketchum@gmail.com>
2017-08-08 13:02:53 -0600

    merge compatibility branch w master

--- tests/test_integration/test_downer.py
+++ tests/test_integration/test_downer.py
@@ -42,7 +42,7 @@ class DownloadTestCase(unittest.TestCase):
                                          dry_run=True)
         self.assertEqual(self.known_scene, scene_list)
 
-    # can't run the download test on travis
+    # can't run the download test on travis.
 
     # def test_downloader(self):
     #     dc.download_landsat((self.start, self.end),
