99d5aaf85f8ab7b3ebe8012fa5023b4025f2eaf9
dgketchum <dgketchum@gmail.com>
2018-09-28 22:49:35 -0600

    working on metadata update

--- landsat/update_landsat_metadata.py
+++ landsat/update_landsat_metadata.py
@@ -15,74 +15,112 @@
 # =============================================================================================
 from __future__ import print_function, absolute_import
 
-import os
 import gzip
-from zipfile import ZipFile
-from numpy import unique
+import os
 from datetime import datetime
+from zipfile import ZipFile
+
 from dask.dataframe import read_csv
+from numpy import unique
 from requests import get
 
-LANDSAT_METADATA_URL = 'http://storage.googleapis.com/gcp-public-data-landsat/index.csv.gz'
-SCENES_ZIP = 'index.csv.gz'
-SCENES = os.path.join(os.path.dirname(__file__), 'scenes')
+fmt = '%Y%m%d'
+date = datetime.strftime(datetime.now(), fmt)
 
+SENTINEL_METADATA_URL = 'https://storage.googleapis.com/gcp-public-data-sentinel-2/index.csv.gz'
+S_SCENES_ZIP = 'l_index.csv.gz'
+S_SCENES = os.path.join(os.path.dirname(__file__), 's_scenes')
+S_TILES_URL = 'https://sentinel.esa.int/documents/247904/1955685/' \
+              'S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml'
+S_TILES_ZIP = 's_tiles.zip'
+S_TILES_DIR = os.path.join(os.path.dirname(__file__), 's_tiles')
+S_LATEST = 's_scenes_{}'.format(date)
+
+LANDSAT_METADATA_URL = 'http://storage.googleapis.com/gcp-public-data-landsat/index.csv.gz'
+L_SCENES_ZIP = 's_index.csv.gz'
+L_SCENES = os.path.join(os.path.dirname(__file__), 'l_scenes')
 WRS_URL = ('https://landsat.usgs.gov/sites/default/files/documents/WRS1_descending.zip',
            'https://landsat.usgs.gov/sites/default/files/documents/WRS2_descending.zip')
 WRS_FILES = (os.path.join(os.path.dirname(__file__), 'wrs', 'wrs1_descending.shp'),
              os.path.join(os.path.dirname(__file__), 'wrs', 'wrs2_descending.shp'))
 WRS_ZIP = 'wrs.zip'
 WRS_DIR = os.path.join(os.path.dirname(__file__), 'wrs')
-
-fmt = '%Y%m%d'
-date = datetime.strftime(datetime.now(), fmt)
-LATEST = 'scenes_{}'.format(date)
-
-PARSE_DATES = ['DATE_ACQUIRED', 'SENSING_TIME']
+L_LATEST = 'l_scenes_{}'.format(date)
 
 
 def update_metadata_lists():
     print('Please wait while Landsat578 updates Landsat metadata files...')
-    if not os.path.isdir(SCENES):
-        os.mkdir(SCENES)
-    os.chdir(SCENES)
-    ls = os.listdir(SCENES)
+    if not os.path.isdir(L_SCENES):
+        os.mkdir(L_SCENES)
+    os.chdir(L_SCENES)
+    ls = os.listdir(L_SCENES)
     for f in ls:
-        if 'scenes_' in f and LATEST not in f:
-            os.remove(os.path.join(SCENES, f))
+        if 'l_scenes_' in f and L_LATEST not in f:
+            os.remove(os.path.join(L_SCENES, f))
     download_latest_metadata()
     split_list()
-    os.remove(LATEST)
-    os.remove(SCENES_ZIP)
-    with open(LATEST, 'w') as empty:
+    os.remove(L_LATEST)
+    os.remove(L_SCENES_ZIP)
+    with open(L_LATEST, 'w') as empty:
+        empty.write('')
+
+    print('Please wait while Landsat578 updates Sentinel metadata files...')
+    if not os.path.isdir(S_SCENES):
+        os.mkdir(S_SCENES)
+    os.chdir(S_SCENES)
+    ls = os.listdir(S_SCENES)
+    for f in ls:
+        if 's_scenes_' in f and S_LATEST not in f:
+            os.remove(os.path.join(S_SCENES, f))
+    download_latest_metadata()
+    split_list()
+    os.remove(S_LATEST)
+    os.remove(S_SCENES)
+    with open(S_LATEST, 'w') as empty:
         empty.write('')
     return None
 
 
 def download_latest_metadata():
-    if not os.path.isfile(SCENES_ZIP):
+    if not os.path.isfile(L_SCENES_ZIP):
         req = get(LANDSAT_METADATA_URL, stream=True)
         if req.status_code != 200:
             raise ValueError('Bad response {} from request.'.format(req.status_code))
 
-        with open(SCENES_ZIP, 'wb') as f:
+        with open(L_SCENES_ZIP, 'wb') as f:
             print('Downloading {}'.format(LANDSAT_METADATA_URL))
             for chunk in req.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
 
-    if not os.path.isfile(LATEST):
-        with gzip.open(SCENES_ZIP, 'rb') as infile:
-            print('unzipping {}'.format(SCENES_ZIP))
-            with open(LATEST, 'wb') as outfile:
+    if not os.path.isfile(L_LATEST):
+        with gzip.open(L_SCENES_ZIP, 'rb') as infile:
+            print('unzipping {}'.format(L_SCENES_ZIP))
+            with open(L_LATEST, 'wb') as outfile:
                 for line in infile:
                     outfile.write(line)
 
+    if not os.path.isfile(S_SCENES_ZIP):
+        req = get(SENTINEL_METADATA_URL, stream=True)
+        if req.status_code != 200:
+            raise ValueError('Bad response {} from request.'.format(req.status_code))
+
+        with open(S_SCENES_ZIP, 'wb') as f:
+            print('Downloading {}'.format(SENTINEL_METADATA_URL))
+            for chunk in req.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+    if not os.path.isfile(L_LATEST):
+        with gzip.open(S_SCENES_ZIP, 'rb') as infile:
+            print('unzipping {}'.format(S_SCENES_ZIP))
+            with open(L_LATEST, 'wb') as outfile:
+                for line in infile:
+                    outfile.write(line)
     return None
 
 
-def split_list(_list=LATEST):
-
+def split_list(_list=L_LATEST):
     print('Please wait while scene metadata is split')
     csv = read_csv(_list, dtype={'PRODUCT_ID': object, 'COLLECTION_NUMBER': object,
                                  'COLLECTION_CATEGORY': object}, blocksize=25e6,
@@ -93,7 +131,7 @@ def split_list(_list=LATEST):
     for sat in sats:
         print(sat)
         df = csv[csv.SPACECRAFT_ID == sat]
-        dst = os.path.join(SCENES, sat)
+        dst = os.path.join(L_SCENES, sat)
         if os.path.isfile(dst):
             os.remove(dst)
         if not os.path.isdir(dst):
